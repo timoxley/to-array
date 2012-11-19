4844e6b99219c3ce84a406509342e4d063e02823
Tim Oxley <secoif@gmail.com>
2012-11-20 09:11:59 +1000

    Add failing empty arguments test.

--- test/to-array.js
+++ test/to-array.js
@@ -51,7 +51,11 @@ describe('identifying collections', function() {
       assert.deepEqual([1, 2], toArray(arguments), 'arguments should be arraylike'); // arguments collection
     })(1, 2)
   })
-
+  it('correctly identifies empty arguments objects', function() {
+    (function() {
+      assert.deepEqual([], toArray(arguments), 'arguments should be arraylike'); // arguments collection
+    })()
+  })
   it('won\'t grab numeric indicies if they do not exist', function() {
     var A = function() {}
     A.prototype.length = 4
