02e5b3a344045ff56a2a3eb6d767fb8e41be6ad0
Jonny Strömberg <jonny.stromberg@gmail.com>
2014-01-15 11:17:25 +0100

    Use component version of mocha and expect

--- test/to-array.js
+++ test/to-array.js
@@ -1,66 +1,66 @@
 var toArray = require('to-array')
-var assert = require('timoxley-assert')
+expect = require('expect.js')
 var domify = require('component-domify')
 
 describe('non-collections', function() {
   describe('converting into arrays', function () {
     it('single element is the function for Functions', function() {
       var func = function(){}
-      assert.deepEqual([func], toArray(func));
+      expect([func]).to.eql(toArray(func));
     })
     it('single element is the number for Numbers', function() {
-      assert.deepEqual([12345], toArray(12345));
+      expect([12345]).to.eql(toArray(12345));
     })
     it('single element is null for null', function() {
-      assert.deepEqual([null], toArray(null));
+      expect([null]).to.eql(toArray(null));
     })
     it('single element is the regex for Regexps', function() {
-      assert.deepEqual([/regexp/], toArray(/regexp/));
+      expect([/regexp/]).to.eql(toArray(/regexp/));
     })
     it('single element is the date for Dates', function() {
       var date = new Date()
-      assert.deepEqual([date], toArray(date));
+      expect([date]).to.eql(toArray(date));
     })
     it('single element is the string for Strings', function() {
-      assert.deepEqual(['string'], toArray('string'));
+      expect(['string']).to.eql(toArray('string'));
     })
   })
 
   it('single element array is empty for nothing', function() {
-    assert.deepEqual([], toArray());
+    expect([]).to.eql(toArray());
   })
   it('detects window is not a collection', function() {
-    assert.deepEqual([window], toArray(window));
+    expect([window]).to.eql(toArray(window));
   })
   it('single element is empty for undefined', function() {
-    assert.deepEqual([], toArray(undefined));
+    expect([]).to.eql(toArray(undefined));
   })
   it('detects `{length: true}` is not a collection', function() {
-    assert.deepEqual([{length: true}], toArray({length: true}));
+    expect([{length: true}]).to.eql(toArray({length: true}));
   })
 })
 
 describe('identifying collections', function() {
   it('correctly identifies arrays', function() {
-    assert.deepEqual([1, 2], toArray([1, 2]));
-    assert.deepEqual([], toArray([]));
+    expect([1, 2]).to.eql(toArray([1, 2]));
+    expect([]).to.eql(toArray([]));
   })
 
   it('correctly identifies arguments objects', function() {
     (function() {
-      assert.deepEqual([1, 2], toArray(arguments), 'arguments should be arraylike'); // arguments collection
+      expect([1, 2]).to.eql(toArray(arguments), 'arguments should be arraylike'); // arguments collection
     })(1, 2)
   })
   it('correctly identifies empty arguments objects', function() {
     (function() {
-      assert.deepEqual([], toArray(arguments), 'arguments should be arraylike'); // arguments collection
+      expect([]).to.eql(toArray(arguments), 'arguments should be arraylike'); // arguments collection
     })()
   })
   it.skip('won\'t grab numeric indicies if they do not exist', function() {
     var A = function() {}
     A.prototype.length = 4
     var a = new A
-    assert.deepEqual([a], toArray(a));
+    expect([a]).to.eql(toArray(a));
   })
 })
 
@@ -69,8 +69,8 @@ describe('testing against dom', function() {
   var blink1, blink2
   before(function() {
     // pick elements that aren't likely to actually appear on the test page
-    blink1 = domify('<blink class="testblink"></blink>')[0]
-    blink2 = domify('<blink class="testblink"></blink>')[0]
+    blink1 = domify('<blink class="testblink"></blink>')
+    blink2 = domify('<blink class="testblink"></blink>')
 
     document.body.appendChild(blink1)
     document.body.appendChild(blink2)
@@ -82,19 +82,19 @@ describe('testing against dom', function() {
   })
 
   it('arrayifys single nodes', function() {
-    assert.deepEqual([blink1], toArray(blink1));
+    expect([blink1]).to.eql(toArray(blink1));
   })
 
   it('detects htmlelement collections are array-like', function() {
-    assert.deepEqual([blink1, blink2], toArray(document.getElementsByTagName('blink')));
+    expect([blink1, blink2]).to.eql(toArray(document.getElementsByTagName('blink')));
   })
 
   it('detects empty collections are array-like', function() {
-    assert.deepEqual([], toArray(document.getElementsByTagName('jkbsdf')));
+    expect([]).to.eql(toArray(document.getElementsByTagName('jkbsdf')));
   })
 
   it('specifically identifies nodelists as a special collection', function() {
-    assert.deepEqual([blink1, blink2], toArray(document.querySelectorAll('.testblink'))); // NodeList
+    expect([blink1, blink2]).to.eql(toArray(document.querySelectorAll('.testblink'))); // NodeList
   })
 })
 
